bc3a702b5d16a5fa9c8fdf37871bd36965908973
itzkodee <orlovskymaksimik@yandex.ru>
2025-06-06 14:39:33 +0000

    Update users.nix
    forgot lol

--- systems/workstation/users.nix
+++ systems/workstation/users.nix
@@ -1,7 +1,7 @@
 {
     # Define a user account. Don't forget to set a password with ‘passwd’.
     # Change name by changing 'admin' to 'joe' for example.
-    users.users.nefavel = {
+    users.users.itzkodee = {
         isNormalUser = true;
         description = "Admin account. Default for potatoFlake";
         extraGroups = [ "networkmanager" "wheel" ]; # If something not working. Check this groups.
